b246a9b4a8da4dbaa79f2159b421380057b077ea
ntpdlp <ntpdlp@gmail.com>
2020-05-06 15:58:31 -0500

    overloading: function_name, parameter

--- syntax/classobject/Overloading.java
+++ syntax/classobject/Overloading.java
@@ -1,4 +1,32 @@
 package basic.syntax.classobject;
 
 public class Overloading {
+
+    private int x;
+    private int y;
+
+    Overloading(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // Overloading: FunctionName || Parameters
+    public int add(){
+        return x+y;
+    }
+    public int add(int z){
+        return x + y + z;
+    }
+
+    public double add(double z){
+        return x + y + z;
+    }
+
+    public static void main(String[] args) {
+        Overloading ol = new Overloading(1,2);
+        System.out.println(ol.add());
+        System.out.println(ol.add(3));
+        System.out.println(ol.add(5.0));
+    }
+
 }
