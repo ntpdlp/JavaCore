196fcd0ad8a9ea4796b900f820dcdb7ac85ddcce
ntpdlp <ntpdlp@gmail.com>
2020-05-06 15:57:46 -0500

    wrapper class, convert String => Integer => int

--- syntax/classobject/wrapperAutobox.java
+++ syntax/classobject/wrapperAutobox.java
@@ -1,4 +1,16 @@
 package basic.syntax.classobject;
 
 public class wrapperAutobox {
+    public static void main(String[] args) {
+        String num1 = "15";
+        String num2 = "5";
+
+        Integer n1 = Integer.parseInt(num1);
+        Integer n2 = Integer.parseInt(num2);
+
+        System.out.println("Add is: " + (n1.intValue()+n2.intValue()));
+        System.out.println("Sub is: " + (n1.intValue()-n2.intValue()));
+        System.out.println("Mul is: " + (n1.intValue()*n2.intValue()));
+        System.out.println("Div is: " + (n1.intValue()/n2.intValue()));
+    }
 }
