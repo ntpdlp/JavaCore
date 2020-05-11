8edf59422fc8faea9892fe93594b7e29a402ab2f
ntpdlp <ntpdlp@gmail.com>
2020-05-11 12:03:29 -0500

    replace inheritance by composition

--- syntax/oop_noinheritance_composition/NoInheritance_CompositionSolution.java
+++ syntax/oop_noinheritance_composition/NoInheritance_CompositionSolution.java
@@ -1,4 +1,42 @@
 package basic.syntax.oop_noinheritance_composition;
 
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "name: " + name + ", age: " + age;
+    }
+}
+
+// Use the composition instead of inheritance
+class PersonWithJob{
+    Person person;
+    int salary;
+
+    PersonWithJob(Person person, int salary){
+        this.person = person;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString(){
+        return person.toString() + ", salary: " + salary;
+    }
+}
+
+
+
 public class NoInheritance_CompositionSolution {
+    public static void main(String[] args) {
+       Person p = new Person("Vo",18);
+       PersonWithJob pwj = new PersonWithJob(p,80000);
+        System.out.println(pwj);
+    }
 }
