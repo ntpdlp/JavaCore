db750a7ea6631db5bc537a5e9340d95c735d93e2
ntpdlp <ntpdlp@gmail.com>
2020-05-16 01:21:33 -0500

    nested class: local class

--- syntax/classinnernested/ParentNestedLocalClass.java
+++ syntax/classinnernested/ParentNestedLocalClass.java
@@ -1,4 +1,24 @@
 package basic.syntax.classinnernested;
 
+class Out{
+    int y = 20;
+    public void display(){
+        class InnerLocal{
+            int x = 5;
+            public void show(){
+                System.out.println("x + y: " + (x+y));
+            }
+        }
+        InnerLocal innerLocal = new InnerLocal();
+        innerLocal.show();
+    }
+}
+
+
+//class in a block or method
 public class ParentNestedLocalClass {
+    public static void main(String[] args) {
+        Out a = new Out();
+        a.display();
+    }
 }
