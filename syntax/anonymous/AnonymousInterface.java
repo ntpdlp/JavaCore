46fd02e46fbfddfe42e951ea71bcc04a5fca4e3b
ntpdlp <ntpdlp@gmail.com>
2020-05-16 21:55:36 -0500

    anonymous interface implementation

--- syntax/anonymous/AnonymousInterface.java
+++ syntax/anonymous/AnonymousInterface.java
@@ -1,4 +1,26 @@
 package basic.syntax.anonymous;
 
+interface Animal{
+    void eat();
+}
+
 public class AnonymousInterface {
+
+    public static void main(String[] args) {
+        Animal duck = new Animal() {
+            public void eat() {
+                System.out.println("duck eat cap cap");
+            }
+        };
+        duck.eat();
+
+        Animal fish = new Animal(){
+            @Override
+            public void eat(){
+                System.out.println("fish eat oc oc oc");
+            }
+        };
+        fish.eat();
+
+    }
 }
