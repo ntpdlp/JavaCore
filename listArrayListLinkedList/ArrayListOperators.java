da4c8e2ddfd658aaa50ce74109803946f64ae277
ntpdlp <ntpdlp@gmail.com>
2020-05-17 17:00:33 -0500

    List is implemented by ArrayList|LinkedList

--- listArrayListLinkedList/ArrayListOperators.java
+++ listArrayListLinkedList/ArrayListOperators.java
@@ -1,4 +1,49 @@
 package basic.listArrayListLinkedList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ArrayListOperators {
+
+    public static void main(String[] args) {
+        //any data type: Object
+        ArrayList randomArrayList = new ArrayList();
+        randomArrayList.add(1);
+        randomArrayList.add("Hello");
+        randomArrayList.add("World");
+        randomArrayList.add(17.5f);
+        randomArrayList.add(89.0);
+        for(Object obj:randomArrayList){
+            System.out.print(obj + "  ");
+        }
+        System.out.println();
+
+        //specific data type: String
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("Winners");
+        strings.add("never");
+        strings.add("quit");
+        strings.add("Quitter");
+        strings.add("never");
+        strings.add("win");
+        System.out.println(strings);
+        strings.remove(1);
+        strings.remove("quit");
+        System.out.println(strings);
+
+        ArrayList<Integer> integers = new ArrayList<>(Arrays.asList(12,5,9,17,99,10));
+        Collections.sort(integers);
+        System.out.println(integers);
+
+        //declare ArrayList asList
+        ArrayList<String> strings1 = new ArrayList<>(Arrays.asList("Winner", "never", "quit", "and", "Quitter", "never", "win"));
+        Collections.sort(strings1);
+        System.out.println("Ascend order: " + strings1);
+        int pos = Collections.binarySearch(strings1,"Winner");
+        System.out.println("Winner binarySearch: " + pos);
+        Collections.sort(strings1,Collections.reverseOrder());
+        System.out.println("Descend order: " + strings1);
+    }
+
 }
--- listArrayListLinkedList/LinkedListOperators.java
+++ listArrayListLinkedList/LinkedListOperators.java
@@ -1,4 +1,31 @@
 package basic.listArrayListLinkedList;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class LinkedListOperators {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedLists = new LinkedList<>();
+        linkedLists.add(10);
+        linkedLists.add(2);
+        linkedLists.add(500);
+        linkedLists.add(257);
+        linkedLists.add(8);
+        System.out.println("unsorted: " + linkedLists);
+        Collections.sort(linkedLists);
+        System.out.println("sorted: " + linkedLists);
+        linkedLists.remove(1);
+        linkedLists.removeFirst();
+        linkedLists.removeLast();
+        System.out.println(linkedLists);
+
+        //init with value
+        LinkedList<Float> floats = new LinkedList<>(Arrays.asList(12.0f,35.2f,3.0f,0.0f, 60.7f));
+        Collections.sort(floats);
+        System.out.println(floats);
+        int pos = Collections.binarySearch(floats,12.0f);
+        System.out.println("Find 0.0 in list: " + pos);
+
+    }
 }
