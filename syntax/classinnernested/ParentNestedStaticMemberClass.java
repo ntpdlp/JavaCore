afb47d5edf13670a2e81f6cbf9f1132b17cc6330
ntpdlp <ntpdlp@gmail.com>
2020-05-16 21:55:04 -0500

    static inner class

--- syntax/classinnernested/ParentNestedStaticMemberClass.java
+++ syntax/classinnernested/ParentNestedStaticMemberClass.java
@@ -1,4 +1,29 @@
 package basic.syntax.classinnernested;
 
 public class ParentNestedStaticMemberClass {
+    static public String x = "outer";
+
+    static class StaticInnerClass{
+        static String y = "inner";
+
+        public static void staticFunc(){
+            System.out.println("i am static function " + x);
+        }
+
+        public void nonstaticFunc(){
+            System.out.println("i am non-static func " + y);
+        }
+    }
+
+    public void outerFunc(){
+        System.out.println("i am outer function ");
+    }
+
+    public static void main(String[] args) {
+        new ParentNestedStaticMemberClass().outerFunc();
+
+        StaticInnerClass.staticFunc();
+        new StaticInnerClass().nonstaticFunc();
+
+    }
 }
