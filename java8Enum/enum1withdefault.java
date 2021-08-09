0446ecf83eca47b9a29968c23c3b859abc18c8d9
ntpdlp <ntpdlp@gmail.com>
2021-08-09 10:41:13 -0500

    enum with default value, request constructor

---
+++ java8Enum/enum1withdefault.java
@@ -0,0 +1,52 @@
+package java8Enum;
+
+//enum is special class, with 3 instances below:
+enum Size {
+	SMALL(0), MEDIUM(1), LARGE(2);
+	
+	int intval;
+	
+	//need implement constructor with 1 int argument
+	Size(int i) {
+		intval = i;
+	}
+
+	public int intval() {
+		return intval;
+	}
+};
+
+class Donut{
+	private Size size; 
+	private float price;
+	
+	Donut(Size size, int price){
+		this.size = size;
+		this.price = price;
+	}
+	
+	public float getPrice() {return this.price;}
+	public void applyDiscount(float percent) {
+		this.price = this.price * (1- percent);
+	}
+	
+	@Override
+	public String toString() {
+		return "{size: " + this.size + " - " + this.size.intval + ", price: " + this.price + "}";
+	}
+}	
+
+public class enum1withdefault {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Donut d1 = new Donut(Size.LARGE, 20);
+		System.out.println("Before : " + d1.toString());
+		d1.applyDiscount((float)0.2);
+		System.out.println("After : " + d1.toString());
+	}
+	
+	
+
+
+}
